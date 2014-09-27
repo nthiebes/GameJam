20b473882238eee005f850d26b335ac90bd04e48
mariacasancho <mariacasancho@gmail.com>
2014-09-27 12:10:39 +0200

    pretty basic structure for core

--- js/dest/compiled.js
+++ js/dest/compiled.js
@@ -1,1 +1,73 @@
-console.log('Get the ball rollin!');
+/* Main part of the game */
+var core = function(document){
+
+	function init(){
+	 /* load resources needed */
+	 resources();
+	 /* load enviroment */
+     enviroment();
+	 /* load player interaction */
+	 playerInteraction();
+	 /* load mister G */
+	 prisoner();
+	 /* setting goals of the game */
+	 settingGoals();	 
+   }
+   
+   function resources (){
+   
+	 /* load sprites */
+	 /* load assets */
+	 console.log('resources loaded');
+   }
+      
+   function enviroment (){
+   
+	 /* load map */
+	 /* load interface */
+	 /* load items on the map */
+	 
+	 console.log('enviroment loaded');
+   }
+   
+   function playerInteraction (){
+   
+	 /* load sprites for mouse */
+	 /* load drag and drop */
+	 console.log('player interaction loaded');
+   }
+   
+   function prisoner (){
+   
+	 /* load IA */
+	 /* load object (sprites & movements) */
+	 console.log('prisoner loaded');
+   }
+   
+   function settingGoals (){
+   
+	 /* load time count */
+	 /* setting start button */
+	 startGame();
+	 /* save results */
+	 console.log('Goals done!');
+   }
+   
+   function startGame(){
+	 /* this is when the game is started after pressing the button */
+	 /* making available action items */
+	 /* event listeners started so prisioner starts to move */
+	 /* countdown started */
+	 /* time over */
+	 /* stop everything */
+	 console.log('startGame done!');
+   }
+     
+   
+
+   return {
+      Init: init
+   }
+}(document);
+
+core.Init();
